6be3439865b4d24a77445d752070144ef49eb294
NewImprove <1171377895@qq.com>
2015-11-01 16:39:16 +0800

    添加项目

---
+++ soure/soure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  soure
+//
+//  Created by 马锦航 on 15/11/1.
+//  Copyright © 2015年 马锦航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ soure/soure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  soure
+//
+//  Created by 马锦航 on 15/11/1.
+//  Copyright © 2015年 马锦航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
